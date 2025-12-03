b54603dbb04319ac8dea44d230a9a034a38204d5
Alejandro <abergasov@gmail.com>
2025-12-03 14:25:47 +0100

    entity fix

--- internal/service/webvisu/puppet/src/index.js
+++ internal/service/webvisu/puppet/src/index.js
@@ -11,12 +11,26 @@ global.cpatchaReslove = "";
     dotenv.config()
 
     const browser = await puppeteer.launch({
-        headless: false
+        headless: false,
+        ignoreHTTPSErrors: true,
+        args: [
+            '--ignore-certificate-errors',
+            '--allow-insecure-localhost'
+        ]
     });
-    const user = process.env.USER
-    const pass = process.env.PASS
     const page = await browser.newPage();
-    await page.goto(mainURL);
+    await page.goto(mainURL, {
+        waitUntil: 'networkidle0',
+    });
+    await page.waitForSelector('input[placeholder="Username"]');
+    const inputs = await page.$$('input');
+    const buttons = await page.$$('button');
+
+    // enter credentials
+    await inputs[0].type(process.env.SCRAP_USER);
+    await inputs[1].type(process.env.SCRAP_PASS);
+    await buttons[1].click();
+
     //await browser.close()
 })();
 
